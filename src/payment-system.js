04465c4547a611864a5cebc190319d08cd32a97b
Julioj1 <dbjulioj@gmail.com>
2025-11-12 16:30:50 -0500

    Completed Payment

--- src/payment-system.js
+++ src/payment-system.js
@@ -1,7 +1,18 @@
 /* eslint-disable max-classes-per-file */
 
 class Payment {
-
+  status = "pending";
+  constructor(amount, recipient) {
+    this.amount = amount;
+    this.recipient = recipient;
+  }
+  process() {
+    this.status = "completed";
+    return `Payment of $${this.amount} to ${this.recipient} completed`
+  }
+  getDetails() {
+    return `$${this.amount} to ${this.recipient} - Status: ${this.status}`
+  }
 }
 
 class CreditCardPayment {
